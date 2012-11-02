9d66ca0d71b8dc504c29a182c2371927dafbdbdd
Keng Wei <keng_wei2000@yahoo.com>
2012-11-03 04:01:27 +0800

    Removed unnecessary methods for AppData

--- src/AppData.java
+++ src/AppData.java
@@ -10,33 +10,21 @@ public class AppData implements Serializable {
 	// Attributes
 	private int currentSemester;
 	private HashSet<School> schools;
-	private HashMap<Integer, Professor> professors;
-	private HashMap<Integer, Student> students;
-	private HashMap<Integer, Course> courses;
 	private HashMap<Integer, Semester> semesters;
 	
 	// Constructor
 	public AppData() {
 		this.schools = new HashSet<School>();
-		this.professors = new HashMap<Integer, Professor>();
-		this.students = new HashMap<Integer, Student>();
-		this.courses = new HashMap<Integer, Course>();
 		this.semesters = new HashMap<Integer, Semester>();
 	}
 	
 	// Getters and setters
 	public int getCurrentSemester() { return currentSemester; }
 	public HashSet<School> getSchools() { return schools; }
-	public HashMap<Integer, Professor> getProfessors() { return professors; }
-	public HashMap<Integer, Student> getStudents() { return students; }
-	public HashMap<Integer, Course> getCourses() { return courses; }
 	public HashMap<Integer, Semester> getSemesters() { return semesters; }
 	
 	public void setCurrentSemester(int sem) { this.currentSemester = sem; }
 	public void setSchools(HashSet<School> schools) { this.schools = schools; }
-	public void setProfessors(HashMap<Integer, Professor> profs) { this.professors = profs; }
-	public void setStudents(HashMap<Integer, Student> students) { this.students = students; }
-	public void setCourses(HashMap<Integer, Course> courses) { this.courses = courses; }
 	public void setSemesters(HashMap<Integer, Semester> semesters) { this.semesters = semesters; }
 	
 	// Specific methods
